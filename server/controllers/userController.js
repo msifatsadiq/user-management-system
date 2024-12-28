7eda48ab6a3fdf4899d8a4fc210e801106c2c02e
msifatsadiq <msifatsadiq@gmail.com>
2024-12-28 21:27:26 +0600

    adding the delete user func

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -130,3 +130,97 @@ exports.edit = (req, res) => {
     );
   });
   };
+// update user
+
+exports.update = (req, res) => {
+const { first_name, last_name, email, phone, comments } = req.body;
+
+
+   pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Database connection failed:", err);
+      return res.status(500).send("Database connection failed"); // Send response and exit
+    }
+    console.log("Connected to DB as ID " + connection.threadId);
+    // get the data from the form
+    let searchItem = req.body.search;
+    // Query the database
+    connection.query(
+      "UPDATE users SET first_name = ?, last_name = ? WHERE id = ?",
+      [first_name,last_name,req.params.id],
+      (err, rows) => {
+        // Release the connection back to the pool
+        connection.release();
+        if (err) {
+          console.error("Error executing query:", err);
+          return res.status(500).send("Error fetching users"); // Send response and exit
+        }
+        console.log("The data from user table:\n", rows);
+        // Render the view with data
+        
+
+        pool.getConnection((err, connection) => {
+          if (err) {
+            console.error("Database connection failed:", err);
+            return res.status(500).send("Database connection failed"); // Send response and exit
+          }
+          console.log("Connected to DB as ID " + connection.threadId);
+          // get the data from the form
+          let searchItem = req.body.search;
+          // Query the database
+          connection.query(
+            "SELECT * FROM users WHERE id = ? ",
+            [req.params.id],
+            (err, rows) => {
+              // Release the connection back to the pool
+              connection.release();
+              if (err) {
+                console.error("Error executing query:", err);
+                return res.status(500).send("Error fetching users"); // Send response and exit
+              }
+              console.log("The data from user table:\n", rows);
+              // Render the view with data
+              res.render("editUser", { rows ,alert:`${first_name} has been updated`});
+            }
+          );
+        });
+
+
+
+      }
+    );
+  });
+  };
+
+
+
+  // delete user
+
+exports.delete = (req, res) => {
+  //    res.render('editUser')
+  
+     pool.getConnection((err, connection) => {
+      if (err) {
+        console.error("Database connection failed:", err);
+        return res.status(500).send("Database connection failed"); // Send response and exit
+      }
+      console.log("Connected to DB as ID " + connection.threadId);
+      // Query the database
+      connection.query(
+        "DELETE FROM users WHERE id = ? ",
+        [req.params.id],
+        (err, rows) => {
+          // Release the connection back to the pool
+          connection.release();
+          if (err) {
+            console.error("Error executing query:", err);
+            return res.status(500).send("Error fetching users"); // Send response and exit
+          }
+          console.log("The data from user table:\n", rows);
+          // Render the view with data
+          // res.render("home", { rows });
+          res.redirect('/')
+        }
+      );
+    });
+    };
--- server/route/user.js
+++ server/route/user.js
@@ -8,6 +8,8 @@ router.post('/',userController.find)
 router.get('/adduser',userController.form)
 router.post('/adduser',userController.create)
 router.get('/edituser/:id',userController.edit)
+router.post('/edituser/:id',userController.update)
+router.get('/:id',userController.delete)
 
 // Routes
 router.get('/', (req, res) => {
